0daab31b19cb5bdb8eede3d3316d6b402c6c925b
SibinStojanovicRA168-2018 <sibin.stojanovic@gmail.com>
2023-01-26 18:21:27 +0100

    added PowerShell script for running frontend app

---
+++ run_react_app.ps1
@@ -0,0 +1,24 @@
+﻿$nodePath = (where node).Path
+if (-not $nodePath) {
+    Write-Host "Installing Node.js..."
+    Invoke-WebRequest -Uri https://nodejs.org/dist/v18.13.0/node-v18.13.0-x64.msi -OutFile nodejs.msi
+    Start-Process msiexec.exe -ArgumentList "/i nodejs.msi /quiet", "/log nodejs-install.log" -Wait
+    Remove-Item -Path nodejs.msi
+}
+elseif (-not (Test-Path $nodePath)) {
+    Write-Host "Installing Node.js..."
+    Invoke-WebRequest -Uri https://nodejs.org/dist/v18.13.0/node-v18.13.0-x64.msi -OutFile nodejs.msi
+    Start-Process msiexec.exe -ArgumentList "/i nodejs.msi /quiet", "/log nodejs-install.log" -Wait
+    Remove-Item -Path nodejs.msi
+}
+
+
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+cd "$scriptPath\isa-front"
+
+npm install webpack-dev-server@latest --save-dev
+
+npm install
+
+npm start
